f73c55a9157859985030c8cd826eb0a3633b6a2b
Junhan <junhan.3008@gmail.com>
2023-06-13 15:32:06 +0800

    feat: mongoose and mongodb connection

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+const connection = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}/?retryWrites=true&w=majority`;
 app.get('/', (req, res) => {
     res.send({ id: 1, name: 'junhan' }).json;
 });
-app.listen(port, () => {
-    console.log('Listening to port: ', port);
-});
+mongoose.connect(connection).then(() => {
+    console.log('MongoDB connected');
+    app.listen(port, () => {
+        console.log('Listening to port: ', port);
+    });
+}).catch(console.error);
 //# sourceMappingURL=index.js.map
