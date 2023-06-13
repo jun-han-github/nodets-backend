24bab8bce159163c5995d86c65eb582813b88c5b
Junhan <junhan.3008@gmail.com>
2023-06-13 20:11:14 +0800

    fix: file name typo

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createUser, getUsers } from './controllers/UserController.js';
+import { createUser, getUsers } from './controllers/userController.js';
 
 const router = express.Router();
 
